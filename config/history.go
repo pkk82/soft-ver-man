d9067d5f8ed32e9e9187d028283676eebe12c273
pkk82@d2018-linux <piotrkrzysztofkozak@gmail.com>
2024-04-20 12:45:59 +0200

    Display installed versions

--- config/history.go
+++ config/history.go
@@ -1,8 +1,12 @@
 package config
 
 import (
+	"fmt"
 	"github.com/pkk82/soft-ver-man/domain"
+	"github.com/pkk82/soft-ver-man/util/console"
 	"github.com/spf13/viper"
+	"os"
+	"text/tabwriter"
 )
 
 func ReadHistoryConfig(name string) (domain.PackageHistory, error) {
@@ -17,4 +21,33 @@ func WriteHistoryConfig(history domain.PackageHistory) error {
 	json := history.Serialize()
 	viper.Set(history.Name+PackageHistorySuffix, json)
 	return viper.WriteConfig()
+}
+
+func DisplayHistory(name string) {
+
+	history, err := ReadHistoryConfig(name)
+	if err != nil {
+		console.Fatal(err)
+	}
+
+	w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', tabwriter.Debug)
+	_, err = fmt.Fprintln(w, "Version\t Main\t Path")
+	if err != nil {
+		console.Fatal(err)
+	}
+	for _, item := range history.Items {
+		main := ""
+		if item.Main {
+			main = " Yes"
+		}
+		_, err = fmt.Fprintf(w, "%s\t %s\t %s\n", item.Version, main, item.Path)
+		if err != nil {
+			console.Fatal(err)
+		}
+	}
+	err = w.Flush()
+	if err != nil {
+		console.Fatal(err)
+	}
+
 }
